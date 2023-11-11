bc2bedd4ae5302af69c10f6363c3836b2ef112c8
dreezy305 <bankoleidris@gmail.com>
2023-11-10 23:45:51 -0800

    set ups

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,20 @@
 import React, { ReactNode, useState } from "react";
-import SideBar from "../SideBar/SideBar";
 import Header from "../Header/Header";
+import SideBar from "../SideBar/SideBar";
 
 function Layout({ children }: { children: ReactNode }) {
-  const [rtl, setRtl] = useState(false);
+  // const [rtl, setRtl] = useState(false);
   return (
     <div
       style={{
         display: "flex",
         height: "100vh",
-        direction: rtl ? "rtl" : "ltr",
+        // direction: rtl ? "rtl" : "ltr",
       }}
     >
       <SideBar />
       <main className="header-width">
-        <div style={{  color: "#44596e" }}>
+        <div style={{ color: "#44596e" }}>
           <Header />
           {children}
         </div>
