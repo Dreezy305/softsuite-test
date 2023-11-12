b1b268d2bbfdda62b2e72f3f8dfd9b295583c53b
dreezy305 <bankoleidris@gmail.com>
2023-11-12 00:35:16 -0800

    element page

--- src/pages/Elements.tsx
+++ src/pages/Elements.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import { ReactComponent as Filter } from "../assets/Filter.svg";
 import { ReactComponent as Search } from "../assets/search.svg";
 import "./elements.scss";
 
@@ -46,12 +47,12 @@ function Elements(): JSX.Element {
 
         <div className="d-flex flex-row align-items-center justify-content-between element_row">
           <div className="element_row-left">
-            <div className="input-group search-group">
+            <div className="input-group element_row-left__search me-2">
               <input
                 type="text"
                 className="form-control px-4"
-                placeholder="Search for anything"
-                aria-label="Recipient's username"
+                placeholder="Search for element"
+                aria-label="Search for element"
                 aria-describedby="basic-addon2"
                 style={{ outline: "none", boxShadow: "none" }}
               />
@@ -63,10 +64,12 @@ function Elements(): JSX.Element {
                 <Search />
               </span>
             </div>
-            <button className="btn btn-primary"></button>
+            <button className="btn btn-primary ms-2">
+              <Filter />
+            </button>
           </div>
           <div className="element_row-right">
-            <button className="btn btn-primary"></button>
+            <button className=""></button>
           </div>
         </div>
       </div>
